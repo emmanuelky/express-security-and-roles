094d6fb7a87698a06e80f7e49c0e427740d7a318
Emmanuel Akhere Iyere <iyereemmanuelakhere@Emmanuels-MacBook-Pro.local>
2019-02-04 17:36:21 +0100

    d

--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,30 @@
 module.exports = {
-  isConnected: function(req, res, next){
-    //if we are connected, express defines a req.user
-    if(req.user){
+  // Middleware that redirects the user to '/login' is not connected
+  isConnected: function(req,res,next) {
+    // If we are connected, express defines a req.user
+    if (req.user) {
       next()
     }
-    else{
+    else {
       res.redirect('/auth/login')
     }
-  }
+  },
+  checkAdmin: function(req,res,next) {
+    if (req.user && req.user.role === 'ADMIN') {
+      next()
+    }
+    else {
+      res.redirect('/')
+    }
+  },
+  checkRole: function(role) {
+    return function(req,res,next) {
+      if (req.user && req.user.role === role) {
+        next()
+      }
+      else {
+        res.redirect('/')
+      }
+    }
+  },
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Room = require('../models/Room')
-const {isConnected} = require('../middlewares')
+const { isConnected, checkAdmin, checkRole } = require('../middlewares')
 
 
 router.get('/', (req, res, next) => {
@@ -10,7 +10,8 @@ router.get('/', (req, res, next) => {
 
 
 router.get('/rooms', (req,res,next)=>{
-  Room.find({ isPublished: true })
+  Room.find({ isPublished: true })
+  .populate('_owner') // change the field `_owner` by the document in the db
   .then(rooms => {
     res.render('rooms', {rooms})
   })
@@ -20,25 +21,42 @@ router.get('/rooms', (req,res,next)=>{
 router.get('/add-room', isConnected, (req,res,next)=>{
   res.render('add-room')
 })
-router.post('/add-room', (req,res,next)=>{
+router.post('/add-room', isConnected, (req,res,next)=>{
   Room.create({
     name: req.body.name,
     description: req.body.description,
     isPublished: req.body.isPublished,
+    _owner: req.user._id // req.user is the connected user
   })
   .then(() => {
     res.redirect('/rooms')
   })
   .catch(next)
 })
-router.get('/my-rooms', (req,res,next)=>{
-  Room.find() // TODO: change the filter to only show the right rooms
+router.get('/my-rooms', isConnected, (req,res,next)=>{
+  // Find only the rooms where `_owner` is the connected user _id 
+  Room.find({
+    _owner: req.user._id
+  })
+  .populate('_owner') // change the field `_owner` by the document in the db
   .then(rooms => {
     res.render('rooms', {rooms})
   })
   .catch(next)
 })
 
+router.get('/admin/rooms', 
+  // checkAdmin, 
+  checkRole('ADMIN'),
+  (req,res,next)=>{
+    Room.find()
+    .populate('_owner')
+    .then(rooms => {
+      res.render('rooms', {rooms})
+    })
+    .catch(next)
+  }
+)
 
 
 module.exports = router;
